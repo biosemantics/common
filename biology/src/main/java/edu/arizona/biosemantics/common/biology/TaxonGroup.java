650b4bd9cac9742352dbd17c77ac28840b34b97d
rodenhausen <thomas.rodenhausen@gmail.com>
2014-12-08 13:34:28 -0700

    add displayname

--- biology/src/main/java/edu/arizona/biosemantics/common/biology/TaxonGroup.java
+++ biology/src/main/java/edu/arizona/biosemantics/common/biology/TaxonGroup.java
@@ -1,5 +1,23 @@
 package edu.arizona.biosemantics.common.biology;
 
 public enum TaxonGroup {
-	Algae, Cnidaria, Fossil, Gastropods, Hymenoptera, Plant, Porifera
+	
+	ALGAE("Algae"), CNIDARIA("Cnidaria"), FOSSIL("Fossil"), GASTROPODS("Gastropods"), HYMENOPTERA("Hymenoptera"), PLANT("Plant"), PORIFERA("Porifera");
+	
+	private String displayName;
+	
+	TaxonGroup(String displayName) {
+		this.displayName = displayName;
+	}
+	
+	public TaxonGroup valueFromDisplayName(String displayName) {
+		for(TaxonGroup taxonGroup : values())
+			if(taxonGroup.getDisplayName().equals(displayName))
+				return taxonGroup;
+		return null;
+	}
+	
+	public String getDisplayName() {
+		return displayName;
+	}
 }
