f76f667c7cb0071cbd5de64a32577b068c61edd4
rodenhausen <thomas.rodenhausen@gmail.com>
2015-06-25 09:26:15 -0700

    hash on taxonidentification

--- taxonomy/src/main/java/edu/arizona/biosemantics/common/taxonomy/TaxonIdentification.java
+++ taxonomy/src/main/java/edu/arizona/biosemantics/common/taxonomy/TaxonIdentification.java
@@ -31,4 +31,44 @@ public class TaxonIdentification implements Serializable {
 	public String getDisplayName() {
 		return rankData.toString() + ":" + author + "," + date;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result
+				+ ((rankData == null) ? 0 : rankData.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TaxonIdentification other = (TaxonIdentification) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (rankData == null) {
+			if (other.rankData != null)
+				return false;
+		} else if (!rankData.equals(other.rankData))
+			return false;
+		return true;
+	}
+	
+	
 }
