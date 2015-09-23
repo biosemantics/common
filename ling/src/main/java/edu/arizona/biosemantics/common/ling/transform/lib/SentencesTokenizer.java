29d073284065d0512117bcb461529e71062d1adf
rodenhausen <thomas.rodenhausen@gmail.com>
2015-09-23 11:15:33 -0700

    reverse prev. commit

--- ling/src/main/java/edu/arizona/biosemantics/common/ling/transform/lib/SentencesTokenizer.java
+++ ling/src/main/java/edu/arizona/biosemantics/common/ling/transform/lib/SentencesTokenizer.java
@@ -2,8 +2,6 @@ package edu.arizona.biosemantics.common.ling.transform.lib;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import edu.arizona.biosemantics.common.ling.Token;
 import edu.arizona.biosemantics.common.ling.transform.ITokenizer;
@@ -16,22 +14,11 @@ import edu.arizona.biosemantics.common.ling.transform.ITokenizer;
  */
 public class SentencesTokenizer implements ITokenizer {
 
-	private Pattern wordPunctuationPattern = Pattern.compile("(.+)([\\!,\\.:;\\?])"); //don't put brackets in here
-	
 	@Override
 	public List<Token> tokenize(String text) {
 		List<Token> tokens = new ArrayList<Token>();
-		for(String token : text.split("\\s+")) {
-			
-			Matcher matcher = wordPunctuationPattern.matcher(token);
-			if(matcher.matches()) {
-				String word = matcher.group(1);
-			 	String punctuation = matcher.group(2);
-			 	tokens.add(new Token(word));
-			 	tokens.add(new Token(punctuation));
-			} else {
-				tokens.add(new Token(token));
-			}			
+		for(String token : text.split("(?;|.|!|?)+\\s+")) {
+			tokens.add(new Token(token));
 		}
 		return tokens;
 	}
