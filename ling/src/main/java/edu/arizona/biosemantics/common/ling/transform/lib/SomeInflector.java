5a48cc2e506c69afa6855720bdf4547fd4280923
rodenhausen <thomas.rodenhausen@gmail.com>
2016-12-12 15:54:54 -0700

    pattern syntax exception

--- ling/src/main/java/edu/arizona/biosemantics/common/ling/transform/lib/SomeInflector.java
+++ ling/src/main/java/edu/arizona/biosemantics/common/ling/transform/lib/SomeInflector.java
@@ -5,6 +5,7 @@ import java.util.Hashtable;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import edu.arizona.biosemantics.common.ling.know.IPOSKnowledgeBase;
 import edu.arizona.biosemantics.common.ling.transform.IInflector;
@@ -44,117 +45,121 @@ public class SomeInflector implements IInflector {
 	
 	@Override
 	public String getSingular(String word) {
-		if(!word.matches(".*?[a-zA-Z].*") || word.contains(")") || word.contains("(")) //not a word, e.g 3-5
-			return word;
-		
-		String s = "";
-		//word = word.toLowerCase().replaceAll("\\W", "").trim();
-		word = word.toLowerCase();
-		String originalWord = word;
-		String[] parts = word.split("-"); //may be a good or bad hyphen. find singular for the last part if it is a word
-		String prefix = "";
-		/*if(parts[parts.length-1] not in WordNet, glossary, or ontology){ //Hong TODO
-			word = originalWord.replace("-", "");
-			prefix = "";
-		}else{*/
-		   word = parts[parts.length-1];
-		   prefix = originalWord.replaceFirst(word+"$", "");
-        //}
-        
-
-		// check cache
-		s = singulars.get(word);
-		if (s != null)
-			return prefix+s;
-
-		if(word.matches("\\w+_[ivx-]+")){
-			singulars.put(word, word);
-			plurals.put(word, word);
-			return prefix+word;
-		}
-
-		if(word.matches("[ivx-]+")){
-			singulars.put(word, word);
-			plurals.put(word, word);
-			return prefix+word;
-		}
-		
-		// adverbs
-		Matcher matcher = lyAdverbPattern.matcher(word);
-		if (matcher.matches()) {
-			singulars.put(word, word);
-			plurals.put(word, word);
-			return prefix+word;
-		}
-
-		String wordcopy = new String(word);
-		wordcopy = checkWN4Singular(wordcopy);
-		if (wordcopy != null && wordcopy.length() == 0) {
-			return prefix+word;
-		} else if (wordcopy != null) {
-			singulars.put(word, wordcopy);
-			if (!wordcopy.equals(word))
-				plurals.put(wordcopy, word);
-			return prefix+wordcopy;
-		} else {// word not in wn
-			Matcher m1 = p1.matcher(word);
-			Matcher m2 = p2.matcher(word);
-			Matcher m3 = p3.matcher(word);
-			Matcher m4 = p4.matcher(word);
-			Matcher m5 = p5.matcher(word);
-			Matcher m6 = p6.matcher(word);
-			Matcher m7 = p7.matcher(word);
-			Matcher m75 = p75.matcher(word);
-			Matcher m8 = p8.matcher(word);
-			Matcher m9 = p9.matcher(word);
-			Matcher m10 = p10.matcher(word);
-			Matcher m11 = p11.matcher(word);
-			Matcher m12 = p12.matcher(word);
-
-			if (m1.matches()) {
-				s = m1.group(1) + "y";
-			} else if (m2.matches()) {
-				s = m2.group(1) + "us";
-			} else if (m3.matches()) {
-				s = m3.group(1) + "ium";
-			} else if (m4.matches()) {
-				s = m4.group(1);
-			} else if (m5.matches()) {
-				s = m5.group(1) + "f";
-			} else if (m6.matches()) {
-				s = m6.group(1) + "ex";
-				//if(!inGlossaryOntology(s)) s = m6.group(1)+"ix"; //Hong TODO use glossary and ontology for getSingular 
-				//if(!inGlossaryOntology(s, conn)) s = null;
-			} else if (m7.matches()) {
-				s = m7.group(1);
-			} else if (m75.matches()) {
-				s = word;
-			} else if (m8.matches()) {
-				s = m8.group(1);
-			}else if(m9.matches()){
-				s = m9.group(1)+"um";
-				//if(!inGlossaryOntology(s, conn)) s = m9.group(1)+"on";
-				//if(!inGlossaryOntology(s, conn)) s = null;
-			}else if(m10.matches()){
-				s = m10.group(1);
-			}else if(m11.matches()){
-				s = m11.group(1)+"s";
-				//if(!inGlossaryOntology(s, conn)) s = null;
+		try {
+			if(!word.matches(".*?[a-zA-Z].*") || word.contains(")") || word.contains("(") || word.contains("]") || word.contains("[")) //not a word, e.g 3-5
+				return word;
+			
+			String s = "";
+			//word = word.toLowerCase().replaceAll("\\W", "").trim();
+			word = word.toLowerCase();
+			String originalWord = word;
+			String[] parts = word.split("-"); //may be a good or bad hyphen. find singular for the last part if it is a word
+			String prefix = "";
+			/*if(parts[parts.length-1] not in WordNet, glossary, or ontology){ //Hong TODO
+				word = originalWord.replace("-", "");
+				prefix = "";
+			}else{*/
+			   word = parts[parts.length-1];
+			   prefix = originalWord.replaceFirst(word+"$", "");
+	        //}
+	        
+	
+			// check cache
+			s = singulars.get(word);
+			if (s != null)
+				return prefix+s;
+	
+			if(word.matches("\\w+_[ivx-]+")){
+				singulars.put(word, word);
+				plurals.put(word, word);
+				return prefix+word;
+			}
+	
+			if(word.matches("[ivx-]+")){
+				singulars.put(word, word);
+				plurals.put(word, word);
+				return prefix+word;
 			}
 			
-			if(s==null & m12.matches()){
-				s = m12.group(1)+"is";
-				//if(!inGlossaryOntology(s, conn)) s = null;
+			// adverbs
+			Matcher matcher = lyAdverbPattern.matcher(word);
+			if (matcher.matches()) {
+				singulars.put(word, word);
+				plurals.put(word, word);
+				return prefix+word;
 			}
-
-			if (s != null) {
-				singulars.put(word, s);
-				if (!s.equals(word))
-					plurals.put(s, word);
-				return prefix+s;
+	
+			String wordcopy = new String(word);
+			wordcopy = checkWN4Singular(wordcopy);
+			if (wordcopy != null && wordcopy.length() == 0) {
+				return prefix+word;
+			} else if (wordcopy != null) {
+				singulars.put(word, wordcopy);
+				if (!wordcopy.equals(word))
+					plurals.put(wordcopy, word);
+				return prefix+wordcopy;
+			} else {// word not in wn
+				Matcher m1 = p1.matcher(word);
+				Matcher m2 = p2.matcher(word);
+				Matcher m3 = p3.matcher(word);
+				Matcher m4 = p4.matcher(word);
+				Matcher m5 = p5.matcher(word);
+				Matcher m6 = p6.matcher(word);
+				Matcher m7 = p7.matcher(word);
+				Matcher m75 = p75.matcher(word);
+				Matcher m8 = p8.matcher(word);
+				Matcher m9 = p9.matcher(word);
+				Matcher m10 = p10.matcher(word);
+				Matcher m11 = p11.matcher(word);
+				Matcher m12 = p12.matcher(word);
+	
+				if (m1.matches()) {
+					s = m1.group(1) + "y";
+				} else if (m2.matches()) {
+					s = m2.group(1) + "us";
+				} else if (m3.matches()) {
+					s = m3.group(1) + "ium";
+				} else if (m4.matches()) {
+					s = m4.group(1);
+				} else if (m5.matches()) {
+					s = m5.group(1) + "f";
+				} else if (m6.matches()) {
+					s = m6.group(1) + "ex";
+					//if(!inGlossaryOntology(s)) s = m6.group(1)+"ix"; //Hong TODO use glossary and ontology for getSingular 
+					//if(!inGlossaryOntology(s, conn)) s = null;
+				} else if (m7.matches()) {
+					s = m7.group(1);
+				} else if (m75.matches()) {
+					s = word;
+				} else if (m8.matches()) {
+					s = m8.group(1);
+				}else if(m9.matches()){
+					s = m9.group(1)+"um";
+					//if(!inGlossaryOntology(s, conn)) s = m9.group(1)+"on";
+					//if(!inGlossaryOntology(s, conn)) s = null;
+				}else if(m10.matches()){
+					s = m10.group(1);
+				}else if(m11.matches()){
+					s = m11.group(1)+"s";
+					//if(!inGlossaryOntology(s, conn)) s = null;
+				}
+				
+				if(s==null & m12.matches()){
+					s = m12.group(1)+"is";
+					//if(!inGlossaryOntology(s, conn)) s = null;
+				}
+	
+				if (s != null) {
+					singulars.put(word, s);
+					if (!s.equals(word))
+						plurals.put(s, word);
+					return prefix+s;
+				}
 			}
+			return prefix+word;
+		} catch(PatternSyntaxException e) {
+			return word;
 		}
-		return prefix+word;
 	}
 	
 	public String checkWN4Singular(String word) {
