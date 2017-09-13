10b3e5d3015865faef2776aade1e837162aafe62
hongcui <hong1.cui@gmail.com>
2017-09-13 10:18:13 -0700

    updates

--- log/src/main/java/edu/arizona/biosemantics/common/log/ILoggable.java
+++ log/src/main/java/edu/arizona/biosemantics/common/log/ILoggable.java
@@ -5,6 +5,6 @@ package edu.arizona.biosemantics.common.log;
 */
 public interface ILoggable { 
 	//should not need this: 
-	public void log(LogLevel level, String msg);
+	//public void log(LogLevel level, String msg);
 	
 }
